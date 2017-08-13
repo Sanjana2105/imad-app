c151edc2235805dcd68a9c6d67416a366816958a
Sanjana2105 <ssanjana.indya@gmail.com>
2017-08-13 19:55:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
-console.log('Loaded!');
+var button=document.getElementBYId('counter');
+button.onClick=function(){
+    var request=new XMLHttpRequest();
+    request.onreadystatechange=function(){
+        if(request.readystatechange===XMLHttpRequest.DONE){
+            if(request.status===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://ssanjanaindya.imad.hasura-app.io/counter',true);
+};
+
